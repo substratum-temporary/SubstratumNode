be794ab91348de37ee195a7819e622d0834e4b89
substratum-temporary <54410899+substratum-temporary@users.noreply.github.com>
2019-08-28 13:12:34 -0400

    111: First passing build
    * Moved successful Chrome installation to third stage
    
    * 111: Installing xvfb in the third stage now, not the first
    
    * All four of these stages have passed at some point

---
+++ ci/install_ui_test_toolchain.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -ev
+# Copyright (c) 2017-2019, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
+sudo apt update
+sudo apt install -y google-chrome-stable xvfb
