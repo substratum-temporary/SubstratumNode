6c068db77ac70f016dcebdfcd37351e44091f182
Substratum Services <substratumservices+1@gmail.com>
2019-09-04 15:37:52 -0400

    111: Put TARGET back in as CACHE_TARGET, factored out some commonality

--- ci/install_node_toolchain.sh
+++ ci/install_node_toolchain.sh
@@ -1,26 +1,30 @@
 #!/bin/bash -xev
 # Copyright (c) 2017-2019, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
 
-RUSTUP_HOME="$1"
-if [[ "$RUSTUP_HOME" == "" ]]; then
-  RUSTUP_HOME="$HOME"
+if [[ "$1" == "" ]]; then
+  CACHE_TARGET="$HOME"
+else
+  CACHE_TARGET="$1"
 fi
 
 function install_linux_macOS() {
   curl https://sh.rustup.rs -sSf | bash -s -- -y
-  "$HOME/.cargo/bin/rustup" update
-  "$HOME/.cargo/bin/rustup" component add rustfmt
-  "$HOME/.cargo/bin/rustup" component add clippy
-  "$HOME/.cargo/bin/cargo" install sccache
+  common
 }
 
 function install_windows() {
   curl https://win.rustup.rs -sSf > /tmp/rustup-init.exe
   /tmp/rustup-init.exe -y
+  common
+}
+
+function common() {
   "$HOME/.cargo/bin/rustup" update
   "$HOME/.cargo/bin/rustup" component add rustfmt
   "$HOME/.cargo/bin/rustup" component add clippy
   "$HOME/.cargo/bin/cargo" install sccache
+
+  cp -R "$HOME/.cargo" "$CACHE_TARGET/.cargo"
 }
 
 case "$OSTYPE" in
