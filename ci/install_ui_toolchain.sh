2ac3b75028a9327789e19e21f527c881fce0e796
Substratum Services <substratumservices+1@gmail.com>
2019-09-05 16:55:29 -0400

    111: Better caching for Linux and macOS?

--- ci/install_ui_toolchain.sh
+++ ci/install_ui_toolchain.sh
@@ -14,7 +14,10 @@ else
 fi
 
 function install_linux() {
-  rm -r "$HOME/.nvm" || echo "node.js not installed"
+  rm "/usr/bin/node" || echo "node.js not installed"
+  rm "/usr/bin/npm" || echo "npm not installed"
+  rm -r "$HOME/.nvm" || echo "node.js configuration not installed"
+  rm -r "/usr/lib/node_modules" || echo "No node_modules installed"
   curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
   sudo apt-get update
   sudo apt-get install -y nodejs
@@ -22,10 +25,14 @@ function install_linux() {
   nvm install "$NODE_VERSION"
 
   mkdir -p "$CACHE_TARGET/usr/bin"
+  mkdir -p "$CACHE_TARGET/usr/lib"
   cp "/usr/bin/node" "$CACHE_TARGET/usr/bin/node"
+  cp "/usr/bin/node" "$CACHE_TARGET/usr/bin/npm"
+  cp -R "/usr/lib/node_modules/" "$CACHE_TARGET/usr/lib"
   cp -R "$HOME/.nvm" "$CACHE_TARGET/.nvm"
 
-  rm -r "$HOME/.yarn" || echo "yarn not installed"
+  rm "/usr/bin/yarn" || echo "yarn not installed"
+  rm -r "$HOME/.yarn" || echo "yarn configuration not installed"
   curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
   sudo apt-get update
@@ -35,16 +42,25 @@ function install_linux() {
 }
 
 function install_macOS() {
-  rm -r "$HOME/.nvm" || echo "node.js not installed"
+  rm "/usr/local/bin/node" || echo "node.js not installed"
+  rm "/usr/local/bin/npm" || echo "npm not installed"
+  rm -r "$HOME/.nvm" || echo "node.js configuration not installed"
+  rm -r "/usr/lib/node_modules" || echo "No node_modules installed"
   brew install node || echo "node.js is already installed"
   source "$HOME/.nvm/nvm.sh"
   nvm install "$NODE_VERSION"
 
+  mkdir -p "$CACHE_TARGET/usr/local/bin"
+  mkdir -p "$CACHE_TARGET/usr/local/lib"
+  cp "/usr/local/bin/node" "$CACHE_TARGET/usr/local/bin/node"
+  cp "/usr/local/bin/node" "$CACHE_TARGET/usr/local/bin/npm"
+  cp -R "/usr/local/lib/node_modules/" "$CACHE_TARGET/usr/local/lib"
   cp -R "$HOME/.nvm" "$CACHE_TARGET/.nvm"
 
   rm -r "$HOME/.yarn" || echo "yarn not installed"
   npm install -g yarn
 
+  cp "/usr/local/bin/yarn" "$CACHE_TARGET/usr/local/bin/yarn"
   cp -R "$HOME/.yarn" "$CACHE_TARGET/.yarn"
 }
 
