8396f41fb95df528822ade0c757e3a6c62b79c90
substratum-temporary <54410899+substratum-temporary@users.noreply.github.com>
2019-09-04 23:02:05 -0400

    Removed Windows temporarily (#18)
    * Removed Windows temporarily
    
    * 111: Trying Kevin's idea
    
    * 111: Removed branch restriction
    
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * See if we can build on multiple platforms
    
    * 111: Trying to run a bash script on all three platforms
    
    * 111: Minor correction
    
    * Experimenting with multiple platforms and multiple stages
    
    * 111: Separate pool for each job
    
    * 111: Took out variables tag
    
    * 111: Skepticism abounds...
    
    * 111: Do stages work outside a matrix?
    
    * 111: Will imageName work as a variable?
    
    * Removed sequence contents for bash
    
    * No spaces in stage or job names
    
    * 111: Trying Node toolchain installation
    
    * 111 Slimmed down
    
    * 111: Added quotes around constant cache key
    
    * 111: First try at Windows toolchain installation, key upgrade
    
    * 111: Wups, error in Microsoft documentation
    
    * 111: Trying RUSTUP_HOME for custom install
    
    * 111: Does USERPROFILE have an unexpected value?
    
    * 111: Put TARGET back in as CACHE_TARGET, factored out some commonality
    
    * 111: I hate Windows and the whole disk-letter thing. Argh.
    
    * 111 BASH, not BATCH. Sheesh.
    
    * 111: End-of-day commit
    
    * 111: Trying to acquiesce to Microsoft divahood.
    
    * 111: Do I haaaaave to?
    
    * 111: Single quotes?
    
    * 111: Maybe it's templates?
    
    * 111 Templates?
    
    * 111: Variables?
    
    * 111: It's an escaping issue.
    
    * 111: A little sed in the installation script

--- ci/install_node_toolchain.sh
+++ ci/install_node_toolchain.sh
@@ -1,22 +1,31 @@
 #!/bin/bash -xev
 # Copyright (c) 2017-2019, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
 
-function install_linux_macOS() {
-  if [[ $(which "$HOME/.cargo/bin/rustc") == "" ]]; then
-    curl https://sh.rustup.rs -sSf | bash -s -- -y
-    "$HOME/.cargo/bin/rustup" update
-    "$HOME/.cargo/bin/rustup" component add rustfmt
-    "$HOME/.cargo/bin/rustup" component add clippy
-  fi
+if [[ "$1" == "" ]]; then
+  CACHE_TARGET="$HOME"
+else
+  CACHE_TARGET="$1"
+fi
 
-  if [[ $(which "$HOME/.cargo/bin/sccache") == "" ]]; then
-    "$HOME/.cargo/bin/cargo" install sccache
-  fi
+function install_linux_macOS() {
+  curl https://sh.rustup.rs -sSf | bash -s -- -y
+  common
 }
 
 function install_windows() {
-    echo "Not yet!"
-    exit 1
+  CACHE_TARGET=$(echo $CACHE_TARGET | sed 's|\\|/|g' | sed 's|^\([A-Za-z]\):|/\1|g')
+  curl https://win.rustup.rs -sSf > /tmp/rustup-init.exe
+  /tmp/rustup-init.exe -y
+  common
+}
+
+function common() {
+  "$HOME/.cargo/bin/rustup" update
+  "$HOME/.cargo/bin/rustup" component add rustfmt
+  "$HOME/.cargo/bin/rustup" component add clippy
+  "$HOME/.cargo/bin/cargo" install sccache
+
+  cp -R "$HOME/.cargo" "$CACHE_TARGET/.cargo"
 }
 
 case "$OSTYPE" in
@@ -30,6 +39,7 @@ case "$OSTYPE" in
     install_linux_macOS
     ;;
   *)
+    echo "Unrecognized operating system $OSTYPE"
     exit 1
     ;;
 esac
