bc72bf5ed116d404054e591c23639a2052b6a079
Dan Wiebe <dnwiebe@gmail.com>
2019-09-04 23:35:15 -0400

    111: Attempting another cache

--- ci/install_ui_toolchain.sh
+++ ci/install_ui_toolchain.sh
@@ -1,36 +1,58 @@
 #!/bin/bash -ev
 # Copyright (c) 2017-2019, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
 
+if [[ "$1" == "" ]]; then
+  CACHE_TARGET="$HOME"
+else
+  CACHE_TARGET="$1"
+fi
+
+if [[ "$2" == "" ]]; then
+  NODE_VERSION="10.16.3"
+else
+  NODE_VERSION="$2"
+fi
+
 function install_linux() {
-  if [[ ! -f "$HOME/.nvm/versions/node/v10.16.3/bin/node" ]]; then
-    curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
-    sudo apt-get update
-    sudo apt-get install -y nodejs
-    source "$HOME/.nvm/nvm.sh"
-    nvm install 10.16.3
-  fi
-  if [[ $(which yarn) == "" ]]; then
-    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    sudo apt-get update
-    sudo apt-get install -y yarn
-  fi
+  curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+  sudo apt-get update
+  sudo apt-get install -y nodejs
+  source "$HOME/.nvm/nvm.sh"
+  nvm install "$NODE_VERSION"
+
+  cp -R "$HOME/.nvm" "$CACHE_TARGET/.nvm"
+
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  sudo apt-get update
+  sudo apt-get install -y yarn
+
+  cp -R "$HOME/.yarn" "$CACHE_TARGET/.yarn"
 }
 
 function install_macOS() {
-  if [[ ! -f "$HOME/.nvm/versions/node/v10.16.3/bin/node" ]]; then
-    brew install node || echo "" # Assume that if installation fails, it's because node is already installed
-    source "$HOME/.nvm/nvm.sh"
-    nvm install 10.16.3
-  fi
-  if [[ $(which yarn) == "" ]]; then
-    npm install -g yarn
-  fi
+  brew install node || echo "" # Assume that if installation fails, it's because node is already installed
+  source "$HOME/.nvm/nvm.sh"
+  nvm install "$NODE_VERSION"
+
+  cp -R "$HOME/.nvm" "$CACHE_TARGET/.nvm"
+
+  npm install -g yarn
+
+  cp -R "$HOME/.nvm" "$CACHE_TARGET/.nvm"
 }
 
 function install_windows() {
-    echo "Not yet!"
-    exit 1
+  CACHE_TARGET=$(echo $CACHE_TARGET | sed 's|\\|/|g' | sed 's|^\([A-Za-z]\):|/\1|g')
+  msiexec.exe //a "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-x64.msi" //quiet
+  source "$HOME/.nvm/nvm.sh"
+  nvm install "$NODE_VERSION"
+
+  cp -R "$HOME/.nvm" "$CACHE_TARGET/.nvm"
+
+  npm install -g yarn
+
+  cp -R "$HOME/.nvm" "$CACHE_TARGET/.nvm"
 }
 
 case "$OSTYPE" in
@@ -38,12 +60,13 @@ case "$OSTYPE" in
     install_windows
     ;;
   Darwin | darwin*)
-    install_macOS
+    install_linux_macOS
     ;;
   linux*)
-    install_linux
+    install_linux_macOS
     ;;
   *)
+    echo "Unrecognized operating system $OSTYPE"
     exit 1
     ;;
 esac
