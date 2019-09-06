45d09f3aad257a3a8032cd450499be8305aa0c3d
substratum-temporary <54410899+substratum-temporary@users.noreply.github.com>
2019-09-06 16:18:50 -0400

    111: I think all the installations may be completing properly now (#22)
    * 111: Better caching for Linux and macOS?
    
    * 111: So what will happen if we go past ~/.nvm?
    
    * 111: Okay, let's see some directory listings.
    
    * 111: Okay, where is it?
    
    * 111: Trying some finds
    
    * 111: Slimmed down the finds a little
    
    * Now failed find won't kill job
    
    * 111: Now installing nvm separately on Linux and Mac
    
    * 111: Trying to install yarn with Npm task
    
    * 111: Can we use Azure-standard facilities to install node.js and yarn?
    
    * 111: First try with install_ui_test_toolchain.sh
    
    * 111: Minor correction
    
    * 111: How Windowsy can we get and still stay bashy?
    
    * 111: Git bash calling out to CMD
    
    * 111: What happens without START on Windows?
    
    * 111: trying direct download link for ChromeStandaloneSetup.exe

--- ci/install_ui_toolchain.sh
+++ ci/install_ui_toolchain.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ev
+#!/bin/bash -xev
 # Copyright (c) 2017-2019, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
 
 if [[ "$1" == "" ]]; then
@@ -14,45 +14,27 @@ else
 fi
 
 function install_linux() {
-  rm -r "$HOME/.nvm" || echo "node.js not installed"
-  curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
-  sudo apt-get update
-  sudo apt-get install -y nodejs
-  source "$HOME/.nvm/nvm.sh"
-  nvm install "$NODE_VERSION"
+  rm "/usr/bin/yarn" || echo "yarn not installed"
+  rm -r "$HOME/.yarn" || echo "yarn configuration not installed"
+  sudo npm install -g yarn
 
   mkdir -p "$CACHE_TARGET/usr/bin"
-  cp "/usr/bin/node" "$CACHE_TARGET/usr/bin/node"
-  cp -R "$HOME/.nvm" "$CACHE_TARGET/.nvm"
-
-  rm -r "$HOME/.yarn" || echo "yarn not installed"
-  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-  sudo apt-get update
-  sudo apt-get install -y yarn
-
-  cp -R "$HOME/.yarn" "$CACHE_TARGET/.yarn"
+  cp "/usr/bin/yarn" "$CACHE_TARGET/usr/bin/yarn"
 }
 
 function install_macOS() {
-  rm -r "$HOME/.nvm" || echo "node.js not installed"
-  brew install node || echo "node.js is already installed"
-  source "$HOME/.nvm/nvm.sh"
-  nvm install "$NODE_VERSION"
-
-  cp -R "$HOME/.nvm" "$CACHE_TARGET/.nvm"
-
-  rm -r "$HOME/.yarn" || echo "yarn not installed"
+  rm "/usr/local/bin/yarn" || echo "yarn not installed"
+  rm -r "$HOME/.yarn" || echo "yarn configuration not installed"
   npm install -g yarn
 
-  cp -R "$HOME/.yarn" "$CACHE_TARGET/.yarn"
+  mkdir -p "$CACHE_TARGET/usr/local/bin"
+  cp "/usr/local/bin/yarn" "$CACHE_TARGET/usr/local/bin/yarn"
 }
 
 function install_windows() {
   CACHE_TARGET=$(echo $CACHE_TARGET | sed 's|\\|/|g' | sed 's|^\([A-Za-z]\):|/\1|g')
   rm -r "$HOME/.nvm" || echo "node.js not installed"
   msiexec.exe //a "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-x64.msi" //quiet
-  source "$HOME/.nvm/nvm.sh"
   nvm install "$NODE_VERSION"
 
   cp -R "$HOME/.nvm" "$CACHE_TARGET/.nvm"
