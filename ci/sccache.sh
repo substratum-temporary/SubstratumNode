d8529d53e8683717284fbea1f4f95ef5e8e203ac
Substratum Services <substratumservices+1@gmail.com>
2019-09-09 11:26:05 -0400

    111: not sure where sccache went... does it hurt to try installing it when it's not there?

--- ci/sccache.sh
+++ ci/sccache.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xev
 # Copyright (c) 2017-2019, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
 
+cargo install sccache || echo "sccache already installed"
 sccache --start-server || echo "sccache server already running"
