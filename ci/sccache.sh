74a28f8a39c7186f7287843a8faa27a645bf17cb
Substratum Services <substratumservices+1@gmail.com>
2019-09-10 15:23:14 -0400

    111: Looking in CI_DIR for bashify_workspace.sh doesn't work if CI_DIR isn't set

--- ci/sccache.sh
+++ ci/sccache.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -xev
 # Copyright (c) 2017-2019, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
+CI_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 WORKSPACE="$("$CI_DIR"/bashify_workspace.sh "$1")"
 PATH="$WORKSPACE/.cargo/bin:$PATH"
