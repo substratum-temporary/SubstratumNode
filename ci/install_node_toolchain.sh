b7be7fc555d3264c1fa2d3f3fb3e5c983291aca5
Substratum Services <substratumservices+1@gmail.com>
2019-09-10 15:35:06 -0400

    111: Nailing down the version of Rust

--- ci/install_node_toolchain.sh
+++ ci/install_node_toolchain.sh
@@ -7,6 +7,12 @@ else
   CACHE_TARGET="$1"
 fi
 
+if [[ "$2" == "" ]]; then
+  RUST_VERSION="stable"
+else
+  RUST_VERSION="$2"
+fi
+
 function install_linux_macOS() {
   rm -r "$HOME/.cargo" || echo "Rust cargo not installed on $OSTYPE"
   rm -r "$HOME/.rustup" || echo "Rust rustup not installed on $OSTYPE"
@@ -25,6 +31,7 @@ function install_windows() {
 
 function common() {
   "$HOME/.cargo/bin/rustup" update
+  "$HOME/.cargo/bin/rustup" install "$RUST_VERSION"
   "$HOME/.cargo/bin/rustup" component add rustfmt
   "$HOME/.cargo/bin/rustup" component add clippy
   "$HOME/.cargo/bin/cargo" install sccache
