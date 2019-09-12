efbc714c76be153d17a61800c55a9d31d14ed9a7
Substratum Services <substratumservices+1@gmail.com>
2019-09-12 16:21:49 -0400

    111: attempting to disable firewall for windows build

--- ci/prepare_node_build.sh
+++ ci/prepare_node_build.sh
@@ -1,24 +1,11 @@
 #!/bin/bash -xev
 # Copyright (c) 2017-2019, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
 CI_DIR="$( cd "$( dirname "$0" )" && pwd )"
-#PARENT_DIR="$1"
+TOOLCHAIN_HOME="$1"
 
-TOOLCHAIN_HOME="$("$CI_DIR"/bashify_workspace.sh "$1")"
-export CARGO_HOME="$TOOLCHAIN_HOME/toolchains/.cargo"
-export RUSTUP_HOME="$TOOLCHAIN_HOME/toolchains/.rustup"
-export PATH="$CARGO_HOME/bin:$PATH"
-chmod +x "$CARGO_HOME"/bin/* || echo "Couldn't make .cargo/bin files executable"
-find "$RUSTUP_HOME" -type f -ipath "*\/bin/*" -print0 |xargs -0 -I{} chmod +x "{}" || echo "Couldn't make .rustup/**/bin/* files executable"
+source "$CI_DIR"/environment.sh "$TOOLCHAIN_HOME"
 
 echo "Path: $PATH"
-echo "First directory in PATH:"
-ls -la "$(echo "$PATH" | cut -d: -f1)"
-echo "Contents of TOOLCHAIN_HOME":
-ls -la "$TOOLCHAIN_HOME"
-echo "Contents of RUSTUP_HOME:"
-ls -la "$RUSTUP_HOME"
-echo "settings.toml:"
-cat "$RUSTUP_HOME"/settings.toml
 echo "which rustc: $(which rustc)"
 echo "which rustup: $(which rustup)"
 rustup show
