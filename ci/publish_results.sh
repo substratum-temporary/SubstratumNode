0f2a5d942b43577dff31f452c4b596b5e48037b4
Substratum Services <substratumservices+1@gmail.com>
2019-09-13 10:45:16 -0400

    111: trying build with the publish results script setup to run upon failure

--- ci/publish_results.sh
+++ ci/publish_results.sh
@@ -8,10 +8,10 @@ rm -rf repo || echo "No leftover repo to delete"
 git clone "https://substratum-temporary:$GITHUB_TOKEN@github.com/substratum-temporary/SubstratumNode-results.git" repo
 cd repo
 cp README.md README.md.old
-if [[ "$TRAVIS_PULL_REQUEST_SLUG" == "" ]]; then
-  RESULTS_LABEL="$TRAVIS_BRANCH"
+if [[ "$CURRENT_PULL_REQUEST_SOURCE_BRANCH" == "" ]]; then
+  RESULTS_LABEL="$CURRENT_BUILD_BRANCH"
 else
-  RESULTS_LABEL="$TRAVIS_PULL_REQUEST_SLUG"
+  RESULTS_LABEL="$CURRENT_PULL_REQUEST_SOURCE_BRANCH"
 fi
 NEW_LINE="* $(date -u) - $RESULTS_LABEL - $STATUS: [generated.zip](https://github.com/substratum-temporary/SubstratumNode-results/blob/master/results/$RESULTS_LABEL/generated.zip?raw=true)"
 cat README.md.old | grep -v "$RESULTS_LABEL" > README.md.clean
