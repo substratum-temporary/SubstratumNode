06f1a295424b5fd128a0c877bf54b59f1f6620ee
Substratum Services <substratumservices+1@gmail.com>
2019-09-20 12:35:24 -0400

    111: do we have AzureSignTool in our path?

--- ci/release.sh
+++ ci/release.sh
@@ -83,6 +83,7 @@ case "$OSTYPE" in
         zip -j SubstratumNode-macOS.dmg.zip node-ui/electron-builder-out/SubstratumNode*.dmg
         ;;
    msys)
+        AzureSignTool sign --help
         signtool sign //tr http://timestamp.digicert.com //td sha256 //fd sha256 //i "DigiCert SHA2 Assured ID Code Signing CA" //n "Substratum Services, Inc." //sm "node-ui/electron-builder-out/SubstratumNode*.exe"
         signtool verify //pa "node-ui/electron-builder-out/SubstratumNode*.exe"
 
